a58852161d498af14839fa21272a05e6632f1a28
Filip Johnsen <filipjohnsen97@gmail.com>
2020-10-30 14:40:25 +0100

    chagned

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -45,6 +45,7 @@ export function Sidebar() {
                     referer: query.slug
                 }}
                 handleSubmit={async (values, helpers) => {
+                    console.log(values)
                     const res = await useNetlify(values)
 
                     if (res.status === 200) {
