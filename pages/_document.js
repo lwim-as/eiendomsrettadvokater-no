073ec221450d63ad70e7937ddc036e007bd40a1a
Filip Johnsen <filipjohnsen97@gmail.com>
2020-10-30 17:39:57 +0100

    added google verifaction

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                 <link href={`https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap`} rel="stylesheet"/>
-                <link rel="icon" href="https://lwim.no/eiendomsrettadvokater/wp-content/uploads/sites/3/2019/04/cropped-icon-black-1-32x32.png" sizes="32x32"/>
+                <link rel="icon" href="../icon.png" sizes="32x32"/>
                 </Head>
                 <body>
                     <Main />
