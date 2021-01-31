2fe8bf0efc3798992d319028aafc0643ec27eda1
Haakon Hess <haakon.hess@gmail.com>
2021-01-31 20:28:18 +0100

    Fixed thumbnail support and flex-alignment

--- components/FrontPagePost.js
+++ components/FrontPagePost.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import NextLink from 'next/link'
 
-import { single_inner_content, post_thumbnail, post_content, category_label } from "../styles/FrontPage.module.css"
+import { single_inner_content, post_thumbnail, post_content, category_label } from '../styles/FrontPage.module.css'
 
 export function FrontPagePost({ post, className }) {
     const { slug, title, categories, featuredImage, excerpt } = post
@@ -9,7 +9,9 @@ export function FrontPagePost({ post, className }) {
     return (
         <div className={className}>
             <div className={single_inner_content}>
-                {featuredImage.node.sourceUrl !== undefined ? <img lazy="true" className={post_thumbnail} src={featuredImage.node.sourceUrl} /> : null}
+                {featuredImage?.node.sourceUrl !== undefined ? (
+                    <img lazy='true' className={post_thumbnail} src={featuredImage.node.sourceUrl} />
+                ) : null}
                 <div className={post_content}>
                     <h2>
                         <NextLink href={`/${slug}`}>
@@ -20,7 +22,9 @@ export function FrontPagePost({ post, className }) {
                         <a className={category_label}>{categories.edges[0].node.name}</a>
                     </Link>
                     <div dangerouslySetInnerHTML={{ __html: excerpt }}></div>
-                    <Link href={`/${slug}`}><a>Les mer...</a></Link>
+                    <Link href={`/${slug}`}>
+                        <a>Les mer...</a>
+                    </Link>
                 </div>
             </div>
         </div>
