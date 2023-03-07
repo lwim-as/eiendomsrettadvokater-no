f1dda1b256e1fdeedef3bb14cb7fb81b8d338337
Haakon Hess <haakon.hess@gmail.com>
2023-03-07 19:57:52 +0100

    Removed 2 specific posts

--- components/Footer.js
+++ components/Footer.js
@@ -3,60 +3,67 @@ import { gql, useQuery } from '@apollo/client'
 import Link from 'next/link'
 
 const lastestPostsQuery = gql`
-    {
+  {
     posts(first: 4) {
-        edges {
-            node {
-                slug
-                title
-            }
+      edges {
+        node {
+          slug
+          title
         }
+      }
     }
-}
+  }
 `
 
 export function Footer() {
-    const { data, loading } = useQuery(lastestPostsQuery)
-    return (
-        <footer className={site_footer}>
-            <div className={footer_container}>
-                <div className={footer_content}>
-                    <img src="../images/eiendomsrett-hvit.png" />
-                </div>
-                <div className={footer_content}>
-                    <h4>INFORMASJON</h4>
-                    <h5>Lead Service AS</h5>
-                    <h5>Org.nr 922 997 454</h5>
-                    <h5>Bogstadveien 27B</h5>
-                    <h5>0355 Oslo</h5>
-                </div>
-                <div className={footer_content}>
-                    <h4>SISTE ARTIKLER</h4>
-                    <ul>
-                        {!loading ? data.posts.edges.map(({ node }) => <FooterItem key={node.slug} item={node} />) : null}
-                    </ul>
-                </div>
-                <div className={footer_content}>
-                    <h4>KONTAKT</h4>
-                    <ul>
-                        <FooterItem item={{slug: "cookies", title: "Informasjonskapsler"}}/>
-                        <FooterItem item={{slug: "kontakt", title: "Kontakt"}}/>
-                        <FooterItem item={{slug: "om", title: "Om"}}/>
-                        <FooterItem item={{slug: "personvern", title: "Personvern"}}/>
-                    </ul>
-                </div>
-            </div>
-            <h4 className={copy_right}>Kopirett © 2020 Eiendomsrettadvokater</h4>
-        </footer>
+  const { data, loading } = useQuery(lastestPostsQuery)
+
+  const filteredPosts =
+    !loading &&
+    data.posts.edges.filter(
+      post =>
+        post.node.slug !== 'hvordan-lanet-er-sikret-og-hvorfor-du-burde-velge-lan-med-sikkerhet' &&
+        post.node.slug !== 'oversikt-over-de-ulike-typene-kredittkort'
     )
+
+  return (
+    <footer className={site_footer}>
+      <div className={footer_container}>
+        <div className={footer_content}>
+          <img src='../images/eiendomsrett-hvit.png' />
+        </div>
+        <div className={footer_content}>
+          <h4>INFORMASJON</h4>
+          <h5>Lead Service AS</h5>
+          <h5>Org.nr 922 997 454</h5>
+          <h5>Bogstadveien 27B</h5>
+          <h5>0355 Oslo</h5>
+        </div>
+        <div className={footer_content}>
+          <h4>SISTE ARTIKLER</h4>
+          <ul>{!loading ? filteredPosts.map(({ node }) => <FooterItem key={node.slug} item={node} />) : null}</ul>
+        </div>
+        <div className={footer_content}>
+          <h4>KONTAKT</h4>
+          <ul>
+            <FooterItem item={{ slug: 'cookies', title: 'Informasjonskapsler' }} />
+            <FooterItem item={{ slug: 'kontakt', title: 'Kontakt' }} />
+            <FooterItem item={{ slug: 'om', title: 'Om' }} />
+            <FooterItem item={{ slug: 'personvern', title: 'Personvern' }} />
+          </ul>
+        </div>
+      </div>
+      <h4 className={copy_right}>Kopirett © 2020 Eiendomsrettadvokater</h4>
+    </footer>
+  )
 }
 
 function FooterItem({ item }) {
-    return (
-        <li>
-            <Link href={`/${item.slug}`}>
-                <a>{item.title}</a>
-            </Link>
-        </li>
-    )
+  return (
+    <li>
+      <Link href={`/${item.slug}`}>
+        <a>{item.title}</a>
+      </Link>
+    </li>
+  )
 }
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,80 +1,97 @@
-import Head from "next/head"
-import { useEffect, useState } from "react"
-import CategoryPost from "../../components/CategoryPost"
-import { getCategoryPaths, getCategoryPosts, getPaginatedCategoryPosts } from "../../lib/graphql-api"
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import CategoryPost from '../../components/CategoryPost'
+import { getCategoryPaths, getCategoryPosts, getPaginatedCategoryPosts } from '../../lib/graphql-api'
 
 import { header, content_container, category_content, load_more_btn } from '../../styles/Category.module.css'
 
 function Category({ category }) {
+  const { seo, name, slug, posts } = category
 
-    const { seo, name, slug, posts } = category
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [fetchedData, setFetchedData] = useState([])
+  const [pageInfo, setPageInfo] = useState({})
 
-    const [isLoading, setIsLoading] = useState(false)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [fetchedData, setFetchedData] = useState([])
-    const [pageInfo, setPageInfo] = useState({})
+  useEffect(() => {
+    setPageInfo({
+      hasNextPage: posts.pageInfo.hasNextPage,
+      endCursor: posts.pageInfo.endCursor,
+    })
+  }, [])
 
-    useEffect(() => {
-        setPageInfo({
-            hasNextPage: posts.pageInfo.hasNextPage,
-            endCursor: posts.pageInfo.endCursor
-        })
-    }, [])
+  useEffect(() => {
+    if (currentPage < 2) return
 
-    useEffect(() => {
-        if (currentPage < 2) return
+    async function fetchMorePosts() {
+      setIsLoading(true)
+      if (pageInfo.hasNextPage) {
+        const { posts: nextPosts } = await getPaginatedPosts(pageInfo.endCursor)
 
-        async function fetchMorePosts() {
-            setIsLoading(true)
-            if (pageInfo.hasNextPage) {
-                const { posts: nextPosts } = await getPaginatedPosts(pageInfo.endCursor)
+        setFetchedData([...fetchedData, ...nextPosts.edges])
+        setPageInfo({ hasNextPage: nextPosts.pageInfo.hasNextPage, endCursor: nextPosts.pageInfo.endCursor })
+        setIsLoading(false)
+      }
+    }
+    fetchMorePosts()
+  }, [currentPage])
 
-                setFetchedData([...fetchedData, ...nextPosts.edges])
-                setPageInfo({ hasNextPage: nextPosts.pageInfo.hasNextPage, endCursor: nextPosts.pageInfo.endCursor })
-                setIsLoading(false)
-            }
-        }
-        fetchMorePosts()
-    }, [currentPage])
+  const filteredPosts = posts.edges.filter(
+    post =>
+      post.node.slug !== 'hvordan-lanet-er-sikret-og-hvorfor-du-burde-velge-lan-med-sikkerhet' &&
+      post.node.slug !== 'oversikt-over-de-ulike-typene-kredittkort'
+  )
 
-    return (
-        <>
+  return (
+    <>
+      <Head>
+        <title>{seo.title}</title>
+        <meta name='description' content={seo.metaDesc} />
+      </Head>
 
-            <Head>
-                <title>{seo.title}</title>
-                <meta name="description" content={seo.metaDesc} />
-            </Head>
-
-            <div className={category_content}>
-                <div className={header}>
-                    <h1>{name}</h1>
-                </div>
-                {posts.edges.length === 0 ? <p>No posts to see here...</p> : posts.edges.map(({ node }) => <CategoryPost className={content_container} post={node} categorySlug={slug} key={node.slug} />)}
-                {currentPage > 1 ? fetchedData.map(({ node }) => <CategoryPost className={content_container} post={node} categorySlug={slug} key={node.slug} />) : null}
-                {pageInfo.hasNextPage ? <button className={load_more_btn} disabled={isLoading} onClick={() => setCurrentPage(current => current + 1)}>{isLoading ? "Laster inn fler" : "Last inn fler"}</button> : null}
-            </div>
-        </>
-    )
+      <div className={category_content}>
+        <div className={header}>
+          <h1>{name}</h1>
+        </div>
+        {posts.edges.length === 0 ? (
+          <p>No posts to see here...</p>
+        ) : (
+          filteredPosts.map(({ node }) => (
+            <CategoryPost className={content_container} post={node} categorySlug={slug} key={node.slug} />
+          ))
+        )}
+        {currentPage > 1
+          ? fetchedData.map(({ node }) => (
+              <CategoryPost className={content_container} post={node} categorySlug={slug} key={node.slug} />
+            ))
+          : null}
+        {pageInfo.hasNextPage ? (
+          <button className={load_more_btn} disabled={isLoading} onClick={() => setCurrentPage(current => current + 1)}>
+            {isLoading ? 'Laster inn fler' : 'Last inn fler'}
+          </button>
+        ) : null}
+      </div>
+    </>
+  )
 }
 
 export async function getStaticProps({ params }) {
+  const { category } = await getPaginatedCategoryPosts(params.slug)
 
-    const { category } = await getPaginatedCategoryPosts(params.slug)
-
-    return {
-        props: {
-            category
-        }
-    }
+  return {
+    props: {
+      category,
+    },
+  }
 }
 
 export async function getStaticPaths() {
-    const { categories } = await getCategoryPaths()
+  const { categories } = await getCategoryPaths()
 
-    return {
-        paths: categories.edges.map(({ node }) => `/category/${node.slug}`),
-        fallback: false
-    }
+  return {
+    paths: categories.edges.map(({ node }) => `/category/${node.slug}`),
+    fallback: false,
+  }
 }
 
 export default Category
